d0bb697a47824e87af1dd7597599816290ccdbae
soufianfallah <soufianfll39@gmail.com>
2025-09-25 22:38:33 +0100

    exercise 2

---
+++ exercise2.js
@@ -0,0 +1,5 @@
+/*Exercise 2 
+write a javascript program to get the extension of a filename.*/
+
+const getFileExtension = (str) => str.slice(str.lastIndexOf('.'));
+console.log(getFileExtension('document.html')); 
